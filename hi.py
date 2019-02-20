e1dd28b9c1555c19ccb5b2fb3ae0ced43d9cfa1a
JHRunning <jeonghunn1@gmail.com>
2019-02-21 00:57:00 +0900

    wip 2

--- hi.py
+++ hi.py
@@ -1,45 +1,50 @@
-import keras
-from keras.layers import Conv2D, MaxPooling2D, Flatten
-from keras.layers import Input, LSTM, Embedding, Dense
-from keras.models import Model, Sequential
-from keras.utils import plot_model
+# 0. 사용할 패키지 불러오기
+from keras.utils import np_utils
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import numpy as np
+from numpy import argmax
 
-# First, let's define a vision model using a Sequential model.
-# This model will encode an image into a vector.
-vision_model = Sequential()
-vision_model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(224, 224, 3)))
-vision_model.add(Conv2D(64, (3, 3), activation='relu'))
-vision_model.add(MaxPooling2D((2, 2)))
-vision_model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-vision_model.add(Conv2D(128, (3, 3), activation='relu'))
-vision_model.add(MaxPooling2D((2, 2)))
-vision_model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-vision_model.add(Conv2D(256, (3, 3), activation='relu'))
-vision_model.add(Conv2D(256, (3, 3), activation='relu'))
-vision_model.add(MaxPooling2D((2, 2)))
-vision_model.add(Flatten())
+# 1. 데이터셋 생성하기
 
-# Now let's get a tensor with the output of our vision model:
-image_input = Input(shape=(224, 224, 3))
-encoded_image = vision_model(image_input)
+# 훈련셋과 시험셋 불러오기
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Next, let's define a language model to encode the question into a vector.
-# Each question will be at most 100 word long,
-# and we will index words as integers from 1 to 9999.
-question_input = Input(shape=(100,), dtype='int32')
-embedded_question = Embedding(input_dim=10000, output_dim=256, input_length=100)(question_input)
-encoded_question = LSTM(256)(embedded_question)
+# 데이터셋 전처리
+x_train = x_train.reshape(60000, 784).astype('float32') / 255.0
+x_test = x_test.reshape(10000, 784).astype('float32') / 255.0
 
-# Let's concatenate the question vector and the image vector:
-merged = keras.layers.concatenate([encoded_question, encoded_image])
+# 원핫인코딩 (one-hot encoding) 처리
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
 
-# And let's train a logistic regression over 1000 words on top:
-output = Dense(1000, activation='softmax')(merged)
+# 훈련셋과 검증셋 분리
+x_val = x_train[:42000] # 훈련셋의 30%를 검증셋으로 사용
+x_train = x_train[42000:]
+y_val = y_train[:42000] # 훈련셋의 30%를 검증셋으로 사용
+y_train = y_train[42000:]
 
-# This is our final model:
-vqa_model = Model(inputs=[image_input, question_input], outputs=output)
+# 2. 모델 구성하기
+model = Sequential()
+model.add(Dense(units=64, input_dim=28*28, activation='relu'))
+model.add(Dense(units=10, activation='softmax'))
 
-# The next stage would be training this model on actual data.
+# 3. 모델 학습과정 설정하기
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
+# 4. 모델 학습시키기
+model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_val, y_val))
 
-plot_model(vqa_model, to_file='model.png')
+# 5. 모델 평가하기
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=32)
+print('')
+print('loss_and_metrics : ' + str(loss_and_metrics))
+
+# 6. 모델 사용하기
+xhat_idx = np.random.choice(x_test.shape[0], 5)
+xhat = x_test[xhat_idx]
+yhat = model.predict_classes(xhat)
+
+for i in range(5):
+    print('True : ' + str(argmax(y_test[xhat_idx[i]])) + ', Predict : ' + str(yhat[i]))
